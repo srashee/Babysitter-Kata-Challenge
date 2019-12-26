1965cfc0859a5a0caeb1c30e4410f97799a41f27
Saad Rasheed <saarasheed@outlook.com>
2019-12-26 10:54:07 -0600

    Time Validation
    Added time validation code to parse start and end time params

--- babysitter.c
+++ babysitter.c
@@ -1,3 +1,25 @@
+#include <stdio.h>
+#include "babysitter.h"
+
 int babysitter(char familyType, int startTime, int endTime) {
-    return 0;
+
+    /* Validate the start time */
+    if ((startTime >= 17 && startTime <= 24) ||
+            (startTime >= 0 && startTime <= 4 )) {
+    }
+    else {
+        printf("Invalid Start Time given \n");
+        return INVALID_START;
+    }
+
+    /* Validate the end time */
+    if ((endTime <= 4 && endTime >= 0) ||
+            (endTime >= 17 && endTime <= 24)) {
+    }
+    else {
+        printf("Invalid End time given \n");
+        return INVALID_END;
+    }
+
+        return 0;
 }
